005f3dc3f3130d7c20d015a6790ff067cb14614f
Wellington Henrique <wellingtonhlc@gmail.com>
2024-07-10 21:15:51 -0300

    Aula 3

---
+++ src/Journey.Application/UseCases/Activities/Register/RegisterActvityValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Journey.Communication.Requests;
+using Journey.Exception;
+
+namespace Journey.Application.UseCases.Activities.Register
+{
+    public class RegisterActvityValidator : AbstractValidator<RequestRegisterActivityJson>
+    {
+        public RegisterActvityValidator()
+        {
+            RuleFor(request => request.Name).NotEmpty().WithMessage(ResourceErrorMessage.NAME_EMPTY);
+        }
+    }
+}
--- src/Journey.Infrastructure/Entities/JourneyDbContext.cs
+++ src/Journey.Infrastructure/Entities/JourneyDbContext.cs
@@ -5,16 +5,17 @@ namespace Journey.Infrastructure.Entities
     public class JourneyDbContext : DbContext
     {
         public DbSet<Trip> Trips { get; set; }
-        //public DbSet<Activity> Activities { get; set; }
+        public DbSet<Activity> Activities { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=D:\\Projetos\\NLW Journey\\src\\Journey.Infrastructure\\JourneyDatabase.db");
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Activity>().ToTable("Activities");
-        }
+        // Esta função pode ser usada em outros tipos de banco como MySQL, SQL server para um join entre Trips e Activities ao atualizar um cadastro de Trip
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Activity>().ToTable("Activities");
+        //}
     }
 }
